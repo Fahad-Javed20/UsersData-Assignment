e9b1b1c4d07f882a0ce7e1f03060bf625202d6a7
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-03 23:33:50 +0500

    Add user dashboard components and Tailwind CSS
    Introduced UserDashboard, UserForm, and UserList components for user management. Replaced default CSS with Tailwind CSS and updated Vite config to include Tailwind plugin. Added UserType definition for user data structure.

---
+++ src/components/UserDashboard.tsx
@@ -0,0 +1,14 @@
+import UserForm from "./UserForm";
+import UserList from "./UserList";
+
+
+const UserDashboard = () => {
+  return (
+    <div>
+        <UserForm/>
+        <UserList/>
+    </div>
+  )
+}
+
+export default UserDashboard;
